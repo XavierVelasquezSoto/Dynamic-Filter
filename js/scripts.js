11db44320af36d34eeab97e392d812fc2cd60317
XavierVelasquezSoto <xaviervelasquez_s@hotmail.com>
2024-11-30 23:55:44 +0100

    update filter

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
 const productsElement = document.getElementById("products");
 
+const amountTextElement = document.getElementById("amount-text");
+
+const sugarlessCheckboxElement = document.getElementById("sugarless-checkbox"); //checkbox products
+
+const orderSelectElement = document.getElementById("order-select"); // order products
+
 const productsCart = [
   {
     name: "Waffle with Berries",
@@ -57,36 +63,99 @@ const productsCart = [
   },
 ];
 
-const newProducts = () => {
+const searchProduct = (product) => {
+  const quantityProduct = productsCart.filter((nameProduct) => {
+    return nameProduct.name.toLowerCase().includes(product.toLowerCase());
+  });
+  // console.log(quantityProduct);
+  newProducts(quantityProduct);
+};
+
+const getProduct = (event) => {
+  const nameProduct = event.target.value;
+  // console.log(nameProduct);
+  searchProduct(nameProduct);
+};
+
+const checkboxSugarless = () => {
+  let filterProduct = "";
+
+  if (sugarlessCheckboxElement.checked) {
+    filterProduct = productsCart.filter((product) => {
+      return product.sugarless === true;
+    });
+  } else {
+    filterProduct = productsCart;
+  }
+
+  newProducts(filterProduct);
+};
+
+const orderSelectProducts = () => {
+  const selectOption = orderSelectElement.value;
+
+  let orderOptions = [...productsCart];
+
+  // console.log(orderOptions);
+
+  if (selectOption === "name") {
+    orderOptions.sort((a, b) => a.name.localeCompare(b.name));
+    console.log(orderOptions);
+  } else if (selectOption === "price") {
+    orderOptions.sort((a, b) => a.price - b.price);
+  }
+
+  newProducts(orderOptions);
+};
+
+// users.sort((a, b) => a.localeCompare(b));
+
+const newProducts = (products) => {
+  productsElement.textContent = "";
+
   fragment = document.createDocumentFragment();
 
-  const newProduct = document.createElement("div");
+  products.forEach((product) => {
+    const newProduct = document.createElement("div");
+    newProduct.classList.add("products-container");
 
-  const newImgProduct = document.createElement("img");
-  newImgProduct.src = `./assets/img/image-waffle.jpg`;
+    const newImgProduct = document.createElement("img");
+    newImgProduct.src = product.img;
 
-  const newSugarlessTittle = document.createElement("h2");
-  newSugarlessTittle.classList.add("sugarlees-title");
-  newSugarlessTittle.textContent = `Waffle with Berries`;
+    const newSugarlessTittle = document.createElement("h2");
+    newSugarlessTittle.classList.add("sugarlees-title");
+    newSugarlessTittle.textContent = product.name;
 
-  const newSugarlessProduct = document.createElement("span");
-  newSugarlessProduct.classList.add("sugarless-product");
-  newSugarlessProduct.textContent = `Sugarless`;
+    const newSugarlessProduct = document.createElement("span");
+    newSugarlessProduct.classList.add("sugarless-product");
+    newSugarlessProduct.textContent = `${product.sugarless}`;
+    if (product.sugarless === true) {
+      newSugarlessProduct.textContent = `Sugarless`;
+    } else if (product.sugarless === false) {
+      newSugarlessProduct.textContent = ``;
+    }
 
-  const newSugarlessPrice = document.createElement("span");
-  newSugarlessPrice.classList.add("sugarless-price");
-  newSugarlessPrice.textContent = `$6.50`;
+    const newSugarlessPrice = document.createElement("span");
+    newSugarlessPrice.classList.add("sugarless-price");
+    newSugarlessPrice.textContent = `$${product.price}`;
 
-  newProduct.append(
-    newImgProduct,
-    newSugarlessTittle,
-    newSugarlessProduct,
-    newSugarlessPrice
-  );
-  fragment.append(newProduct);
-  productsElement.append(fragment);
+    newProduct.append(
+      newImgProduct,
+      newSugarlessTittle,
+      newSugarlessProduct,
+      newSugarlessPrice
+    );
+
+    fragment.append(newProduct);
+    productsElement.append(fragment);
+  });
 };
-newProducts();
+
+newProducts(productsCart);
+
+amountTextElement.addEventListener("input", getProduct);
+sugarlessCheckboxElement.addEventListener("change", checkboxSugarless); // checkbox products
+orderSelectElement.addEventListener("change", orderSelectProducts); //order products
 
 // <div id="products" class="products">
 //   <img src="./assets/img/image-waffle.jpg" alt="" />
